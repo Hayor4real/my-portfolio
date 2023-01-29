6a3441a2d2dd05ed750fd552404d14678e7cc4e0
Akindele Ayorinde Emmanuel <AKINDELEAYORINDE55@GMAIL.COM>
2023-01-29 20:12:35 +0100

    modify the feature file

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,12 +1,34 @@
 import React from "react";
+import { FaReact } from "react-icons/fa";
+import { RiGamepadLine } from "react-icons/ri";
+import { SiAntdesign } from "react-icons/si";
+import Title from "../layouts/Title";
+import Card from "./Card";
 
 const Features = () => {
   return (
     <section
       id="features"
-      className="w-full h-[800px] pb-20 flex border-b-[1px] border-b-black"
+      className="w-full h-[800px] py-20  border-b-[1px] border-b-black"
     >
-      Features
+      <Title title="Features" des="what I Do" />
+      <div className="grid grid-cols-3 gap-20">
+        <Card
+          title="Frontend Development"
+          des="I built some webpages and application using React"
+          icon={<FaReact />}
+        />
+        <Card
+          title="Game development "
+          des="I built some game application using Javascipts"
+          icon={<RiGamepadLine />}
+        />
+        <Card
+          title="Ux design"
+          des="I know the basic of Ux Design and hope to learn more"
+          icon={<SiAntdesign />}
+        />
+      </div>
     </section>
   );
 };
