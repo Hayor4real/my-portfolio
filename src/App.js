0881876cd55e613be89e98acc6f5b4d7ed108975
Akindele Ayorinde Emmanuel <AKINDELEAYORINDE55@GMAIL.COM>
2023-02-09 14:44:07 +0100

    components added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import Banner from "./components/banner/Banner";
 import Features from "./components/features/Features";
 import Projects from "./components/projects/Projects";
 import Resume from "./components/resume/Resume";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="w-full h-auto bg-bodyColor text-lightText px-4">
       <div className="max-w-screen-xl mx-auto px-16">
         <Navbar />
-        <Banner />
-        <Features />
-        <Projects />
-        <Resume />
+        <div className="max-w-screen-xl mx-auto">
+          <Banner />
+          <Features />
+          <Projects />
+          <Resume />
+          <Contact />
+          <Footer />
+        </div>
       </div>
     </div>
   );
